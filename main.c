f0715032029ac9ace628fa2d8f3647448eb14089
김정민 <jeongmin@nhndooray.com>
2022-01-24 17:15:14 +0900

    신입-베이스캠프-9기-루키판다/119 mul() 구현

--- main.c
+++ main.c
@@ -48,8 +48,8 @@ int sub(int number1, int number2)
  
 int multi(int number1, int number2)
 {
-	//TODO implementation
-	return 0;
+	//TODO implementation	
+	return number1 * number2;
 }
  
 int div(int number1, int number2)
